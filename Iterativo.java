d20efbe1c27ed48b562f5ab629b2651e0d09424d
VichoH-32 <kiwi7202v@gmail.com>
2017-05-01 04:22:41 -0300

    2.0 / iteratico no saca area

--- Iterativo.java
+++ Iterativo.java
@@ -4,18 +4,17 @@ public class Iterativo{
 
     Scanner sc=new Scanner(System.in);
 
-    private int area;
+    private int areaI;
     private char[][] mat;
 
     public Iterativo(){
         mat=new char[1][1];
         mat[0][0]='-';
-        area=1;
+        areaI=1;
     }
 
     public Iterativo(int n){
         mat=new char[n][2*n - 1];
-
         for(int i=0; i<mat.length; i++){
             if(i==0){
                 System.out.print("ingrese '#' o '-' para pintarlo o no y luego ENTER: ");
@@ -32,14 +31,94 @@ public class Iterativo{
                 }
             }
         }
-        area=0;
+        areaI=0;
+    }
+    public void setMaxAreaI(){
+        for(int i=0; i<mat.length -1; i++){
+            for(int j=0; j<mat[i].length - 2*i -1; j++){
+                int areaAuxI=0;
+                int c=0;
+                int v=0;
+                boolean bob=true;
+                if(j%2==0){
+                    while(bob=true && v<= 2*c ){
+                        if(i==0){
+                            if(c==0){
+                                if(mat[0][j]=='-'){
+                                    areaAuxI=1;
+                                }else{
+                                    areaAuxI=0;
+                                }
+                            }else{
+                                if(v== 2*c){
+                                    bob=false;
+                                    areaAuxI=areaAuxI + 2*c +1;
+                                }else{
+                                    j++;
+                                    v++;
+                                }
+                            }
+                        }else{
+                            if(mat[j][i]!='-'){
+                                bob=false;
+                            }else{
+                                if(v== 2*c){
+                                    i--;
+                                    j= j - 2*c ;
+                                    areaAuxI= areaAuxI + 2*c +1;
+                                    c++;
+                                    v=0;
+                                }else{
+                                    j++;
+                                    v++;
+                                }
+                            }
+                        }
+                    }
+                    if(areaAuxI>this.areaI){
+                        this.areaI=areaAuxI;
+                    }
+                }else{
+                    while(bob=true && v<= 2*c ){
+                        if( j- (2*c) ==1 || j== mat[i].length - 2*i -2 -v ){
+                            if(mat[i][j]!='-'){
+                                bob=false;
+                            }else{
+                                if(v== 2*c){
+                                    bob=false;
+                                    areaAuxI=areaAuxI + 2*c +1;
+                                }else{
+                                    j--;
+                                    v++;
+                                }
+                            }
+                        }else{
+                            if(mat[i][j]!='-'){
+                                bob=false;
+                            }else{
+                                if(v== 2*c ){
+                                    i++;
+                                    j=j + 2*c ;
+                                    areaAuxI=areaAuxI + 2*c +1;
+                                    c++;
+                                    v=0;
+                                }
+                            }
+                        }
+                    }
+                    if(areaAuxI>this.areaI){
+                        this.areaI=areaAuxI;
+                    }
+                }
+            }
+        }
     }
 
-    public int getAreaI1(){
-        return area;
+    public int getAreaI(){
+        return this.areaI;
     }
 
-    public void printMatI1(){
+    public void printMatI(){
         System.out.println("");//print de orden visual
         for(int i=0; i<mat.length; i++){
             for(int j=0; j<mat[i].length; j++){
--- main2.java
+++ main2.java
@@ -4,21 +4,22 @@ public class main2{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
 
-        /*TIte tri1=new TIte();
+        Iterativo tri1=new Iterativo();
         tri1.printMatI();
         System.out.println("el area del triangulo mas grande sin pintar es "+tri1.getAreaI() );
 
         System.out.print("introducir largo del triangulo ---> ");
         int n = sc.nextInt();
-        TIte tri3=new TIte(n);
+        Iterativo tri3=new Iterativo(n);
         tri3.printMatI();
-        System.out.println("el area del triangulo mas grande sin pintar es "+tri3.getAreaI() );*/
-        System.out.print("ingrese la cantidad de filas ---> ");
+        tri3.setMaxAreaI();
+        System.out.println("el area del triangulo mas grande sin pintar es "+tri3.getAreaI() );
+        /*System.out.print("ingrese la cantidad de filas ---> ");
         int n=sc.nextInt();
         Recursivo tri4=new Recursivo(n);
         tri4.armarR(0,0,"noSeLlama");
         tri4.printR(0,0);
-        tri4.setAreaR(0,0);
-        System.out.println("El triangulo mas grande es de Area "+tri4.getAreaR());
+        tri4.setMaxAreaR(0,0);
+        System.out.println("El triangulo mas grande es de Area "+tri4.getAreaR());*/
     }
 }
