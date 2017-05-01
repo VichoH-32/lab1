4925db2baab6634896fa0185017bc2f2d346bdcd
VichoH-32 <kiwi7202v@gmail.com>
2017-05-01 00:17:02 -0300

    2.0 / compila pero entrega mal el area

--- Recursivo.java
+++ Recursivo.java
@@ -5,6 +5,7 @@ public class Recursivo{
     private int area;
     private char[][] mat;
     private int areaAux;
+    private boolean bob;
 
     //Crea una matriz vacia de n * 2n-1
     public Recursivo(int n){
@@ -24,7 +25,7 @@ public class Recursivo{
 
             //pide nuevos datos y llena el primer elemento de la fila actual
             if(j==0){
-                System.out.print("ingrese una combinacion de '-'s y '#'s de largo "+(mat[i].length- 2*i )+" ---> ");
+                System.out.print("ingrese una combinacion de '-'s y '#'s de largo "+(mat[i].length - 2*i )+" ---> ");
                 letras=sc.nextLine();
                 mat[i][j]=letras.charAt(j);
                 armarR(i, j+1, letras);
@@ -78,9 +79,10 @@ public class Recursivo{
             System.out.print(" ");
             asistentePrintR(i-1);
         }
-    }public void setAreaR(int i, int j){//prueba de corrección
-        setMaxAreaR( i, j);
     }
+    //public void setAreaR(int i, int j){//prueba de corrección
+        //setMaxAreaR( i, j);
+    //}
 
     //calcula el area del triangulo mas grande que encuentre
     public void setMaxAreaR( int i, int j){
@@ -93,33 +95,33 @@ public class Recursivo{
             if( i==mat.length -1){
                 parR( i, j, 0);
                 if(areaAux > this.area){
-                    this.area = areaAux;
+                    this.area = areaAux;//termina la recursión
                 }
 
-            //si no es la ultima fila calcula un area, pregunta si es mayor que el actual y avanza en la siguiente recursión
+            //si no es la ultima fila calcula un area
             }else{
                 parR( i, j, 0);
-                if(areaAux > this.area){
+                if(areaAux > this.area){//pregunta si el area calculada es mayor que la actual
                     this.area = areaAux;
                 }
-                setMaxAreaR( i+1, 0);
+                setMaxAreaR( i+1, 0);// avanza a la siguiente fila
             }
-
-        //
         }else{
-            if( j%2==0){
-                //areaAux=0;
+
+            //revisa si j es par o impar para entrar a su respectivo metodo("par()" o "impar()")
+            if( j%2==0){//si es par
                 parR( i, j, 0);
-                if(areaAux > this.area){
+                System.out.println("salio de parR");////////////
+                if(areaAux > this.area){//pregunta si el area calculada es mayor que la actual
                     this.area= areaAux;
                 }
-                setMaxAreaR( i, j+1);
+                setMaxAreaR( i, j+1); //avanza al siguiente elemento
             }else{
-                imparR( i, j , 0);
-                if(areaAux > area){
+                imparR( i, j , 0);//si es impar
+                if(areaAux > area){//pregunta si el area calculada es mayor a la actual
                     this.area= areaAux;
                 }
-                setMaxAreaR( i, j+1);
+                setMaxAreaR( i, j+1); //avanza al siguiente elemento
             }
         }
     }
@@ -127,59 +129,63 @@ public class Recursivo{
         return this.area;
     }
 
+    //metodo si el elemento actual es par, va sumando area hacia arriba si se cumplen las condiciones
     public void parR( int i, int j, int c){
-        if(i==0){
-            if(c==0){
-                if(mat[i][j]!='-'){
-                    this.areaAux=0;
+        this.bob=true;//booleano("bob") para revisar si entra o no a la siguiente recurción
+        if(i==0){//si esta en la primera fila no seguirá sumando hacia arriba
+            if(c==0){//si contador("c") es 0 significa que empezó a revisar en la primera fila
+                if(mat[i][j]=='-'){//reemplaza areaAux con 1 o 0 si el elemento es "-" o "#" respectivamente
+                    areaAux=1;
                 }else{
-                    this.areaAux=1;
+                    areaAux=0;
                 }
-            }else{
-                this.areaAux= this.areaAux + pop(i,j,c,0);
+            }else{//si c no es 0 significa que empezó a contar desde una fila superior
+                pop(i,j,c,0);//llama al metodo que revisa si hay que agregar area o no
             }
         }else{
-            if(mat[i][j]!='-'){
-                this.areaAux=0;
-            }else{
-                this.areaAux= this.areaAux + pop(i,j,c,0);
-                parR( i-1, j+1, c+1);
+            if(mat[i][j]=='-'){//revisa si el elemento actual es "-" o "#" , si es "#" terminara el metodo
+                pop( i, j, c, 0);
+                if( bob ==true){//si bob sigue siendo true para ver si entra a la siguiente recurción
+                    parR( i-1, j+1, c+1);
+                }
             }
         }
     }
     public void imparR( int i, int j, int c){
+        this.bob=true;
         if( j-c==1 || j+c==mat[i].length - (2*i) -2){
             if( c==0){
-                if(mat[i][j]!='-'){
-                    areaAux=0;
-                }else{
+                if(mat[i][j]=='-'){
                     areaAux=1;
+                }else{
+                    areaAux=0;
                 }
             }else{
-                areaAux= areaAux + pop( i, j, c, 0);
+                pop( i, j, c, 0);
             }
         }else{
-            if(mat[i][j]!=0){
-                areaAux=0;
-            }else{
-                areaAux= areaAux + pop( i, j, c, 0);
-                imparR( i+1, j-1, c+1);
+            if(mat[i][j]=='-'){
+                pop( i, j, c, 0);
+                if(bob==true){
+                    imparR( i+1, j-1, c+1);
+                }
             }
         }
     }
 
-    public static int pop( int i, int j, int c, int v){
+    public void pop( int i, int j, int c, int v){
+        System.out.println("si entro"+(c+1)+"veces");/////////////////////////////
         if(v== 2*c ){
-            if(mat[i][j]!='_'){
-                return 0;
+            if(mat[i][j]=='_'){
+                areaAux=areaAux + 2*c +1 ;
             }else{
-                return (2*c +1);
+                this.bob=false;
             }
         }else{
-            if(mat[i][j-c]!='-'){
-                return 0;
+            if(mat[i][j-c]=='-'){
+                pop( i, j+1, c, v+1);
             }else{
-                return pop(i, j+1, c, v+1);
+                this.bob=false;
             }
         }
     }
